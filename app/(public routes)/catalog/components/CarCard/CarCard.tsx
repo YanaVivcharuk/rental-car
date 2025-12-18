9de0398870806bb3a643c401a1b3dbbc876a6e05
YanaVivcharuk <yanavivcharuk@icloud.com>
2025-12-18 22:55:33 +0100

    edited

--- app/(public routes)/catalog/components/CarCard/CarCard.tsx
+++ app/(public routes)/catalog/components/CarCard/CarCard.tsx
@@ -1,9 +1,9 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-import { Car } from "@/types/car";
-import { useCarStore } from "@/store/carsStore";
-import styles from "./CarCard.module.css";
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Car } from '@/types/car';
+import { useCarStore } from '@/store/carsStore';
+import styles from './CarCard.module.css';
 
 interface CarCardProps {
   car: Car;
@@ -11,16 +11,16 @@ interface CarCardProps {
 }
 
 export default function CarCard({ car, priority = false }: CarCardProps) {
-  const favorites = useCarStore((state) => state.favorites);
-  const addToFavorites = useCarStore((state) => state.addToFavorites);
-  const removeFromFavorites = useCarStore((state) => state.removeFromFavorites);
+  const favorites = useCarStore(state => state.favorites);
+  const addToFavorites = useCarStore(state => state.addToFavorites);
+  const removeFromFavorites = useCarStore(state => state.removeFromFavorites);
 
   const formatMileage = (mileage: number): string => {
-    return mileage.toLocaleString("en-US").replace(/,/g, " ");
+    return mileage.toLocaleString('en-US').replace(/,/g, ' ');
   };
 
   const onClickFavorite = () => {
-    if (favorites.some((favCar) => favCar.id === car.id)) {
+    if (favorites.some(favCar => favCar.id === car.id)) {
       removeFromFavorites(car.id);
     } else {
       addToFavorites(car);
@@ -40,13 +40,13 @@ export default function CarCard({ car, priority = false }: CarCardProps) {
         />
         <button
           type="button"
-          className={`${styles.favoriteBtn} ${favorites.some((favCar) => favCar.id === car.id) ? styles.active : ""}`}
+          className={`${styles.favoriteBtn} ${favorites.some(favCar => favCar.id === car.id) ? styles.active : ''}`}
           onClick={onClickFavorite}
           aria-label="Add to favorites"
         >
           <svg className={styles.favoriteIcon}>
             <use
-              href={`/symbol-defs.svg#${favorites.some((favCar) => favCar.id === car.id) ? "icon-fillHeart" : "icon-defaultHeart"}`}
+              href={`/symbol-defs.svg#${favorites.some(favCar => favCar.id === car.id) ? 'icon-fillHeart' : 'icon-defaultHeart'}`}
             />
           </svg>
         </button>
@@ -55,7 +55,7 @@ export default function CarCard({ car, priority = false }: CarCardProps) {
       <div className={styles.content}>
         <div className={styles.header}>
           <h3 className={styles.title}>
-            {car.brand} <span className={styles.model}>{car.model}</span>,{" "}
+            {car.brand} <span className={styles.model}>{car.model}</span>,{' '}
             {car.year}
           </h3>
           <span className={styles.price}>${car.rentalPrice}</span>
@@ -63,10 +63,10 @@ export default function CarCard({ car, priority = false }: CarCardProps) {
 
         <ul className={styles.carDetails}>
           <li className={styles.carDetailsItem}>
-            {car.address.split(",")[1]?.trim()}
+            {car.address.split(',')[1]?.trim()}
           </li>
           <li className={styles.carDetailsItem}>
-            {car.address.split(",")[2]?.trim()}
+            {car.address.split(',')[2]?.trim()}
           </li>
           <li className={styles.carDetailsItem}>{car.rentalCompany}</li>
           <li className={styles.carDetailsItem}>{car.type}</li>
